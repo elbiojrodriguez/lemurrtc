06b510f74c45a85b5d8af04938657dc6a8944a20
elbiojrodriguez <51291186+elbiojrodriguez@users.noreply.github.com>
2025-07-18 17:39:18 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+{
+  "name": "lemurrtc",
+  "version": "1.0.0",
+  "description": "Servidor de sinalização WebRTC para lemur-dono e lemur-visitante",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "socket.io": "^4.7.2"
+  }
+}
